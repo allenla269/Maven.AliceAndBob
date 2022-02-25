0f7a70719c8bf34f1ce17a8e87217c53bc6773f0
Allen L <allenl@Zipcoders-MBP.fios-router.home>
2022-02-24 20:59:30 -0500

    code completed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args ){
-
+Scanner in = new Scanner(System.in);
+System.out.println("Enter your name: ");
+String newN = in.nextLine();
+if (newN.equalsIgnoreCase("Alice") || newN.equalsIgnoreCase("Bob")) {
+    System.out.println("Hello");
+}
     }
 }
